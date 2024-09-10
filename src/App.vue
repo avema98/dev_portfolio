8398d96d942c871af0b885251594bbaef1c44a76
avema98 <avema@cybersafesolutions.com>
2024-09-09 23:22:56 -0400

    wills base app up

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,133 @@
 <template>
-  <div id="app" class="app-container">
 
+  <div id="app" >
 
-    <nav class="navbar">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
+    <div id="desktopNavbar" style="width: 100px; text-align: center; padding: 10px; font-size:smaller;">
+      <div class="desktopLink" @:click="openWindow('main')">
+        <img src="@/assets/img/site/mz-desktop-icon.png">
+        <p>
+          <span>Home</span>
+        </p>
+      </div>
+      <div class="desktopLink" @:click="openWindow('experience')">
+        <img src="@/assets/img/site/mz-desktop-icon.png">
+        <p>
+          <span>About</span>
+        </p>
+      </div>
+      <div class="desktopLink" @:click="openWindow('projects')">
+        <img src="@/assets/img/site/mz-desktop-icon.png">
+        <p>
+          <span>Projects</span>
+        </p>
+      </div>
+      <div class="desktopLink">
+        <img src="@/assets/img/site/mz-desktop-icon.png">
+        <p>
+          <span>Writing</span>
+        </p>
+      </div>
+      <div class="desktopLink">
+        <img src="@/assets/img/site/mz-desktop-icon.png">
+        <p>
+          <span>Sitemap</span>
+        </p>
+      </div>
+    </div>
+    
+    <div id="windowWrapper">
+      <!-- main window -->
+      <div class="windowBox" v-show="windowsOpen.main" style="left:20vw; top:10vh;">
+        <div class="windowTitleBar">
+          <div class="windowTitleBarText">Home</div>
+          <div class="windowTitleBarControls">
+            <button @:click="toggleContent('main')">
+              <div v-if="windowsContent.main">_</div>
+              <div v-else>+</div>
+            </button>
+            <button @:click="closeWindow('main')">X</button>
+          </div>
+        </div>
+        
+        
+        
+        <div v-show="windowsContent.main" class="section-content" style="width:60vw;">
+          <!-- iframe! -->
+          <iframe src="views/home.html" name="iframe_main" title="Main Window IFrame"></iframe>
+        </div>
+      </div>
 
+      <!-- experience window -->
+      <div class="windowBox" v-show="windowsOpen.experience" style="left:22vw; top:12vh;">
+        <div class="windowTitleBar">
+          <div class="windowTitleBarText">About</div>
+          <div class="windowTitleBarControls">
+            <button @:click="toggleContent('experience')">
+              <div v-if="windowsContent.experience">_</div>
+              <div v-else>+</div>
+            </button>
+            <button @:click="closeWindow('experience')">X</button>
+          </div>
+        </div>
+        <!-- navbar -->
+        <nav v-show="windowsContent.experience" class="navbar">
+          <ul>
+            <li><a href="views/about-me.html" target="iframe_experience">Me</a></li>
+            <li><a href="about.html" target="iframe_experience">Skills</a></li>
+            <li><a href="games.html" target="iframe_experience">Education</a></li>
+            <li><a href="music.html" target="iframe_experience">Work</a></li>
+          </ul>
+        </nav>
+        <div v-show="windowsContent.experience" class="section-content" style="width:60vw;">
+          <!-- iframe! -->
+          <iframe src="home.html" name="iframe_experience" title="Experience Window IFrame"></iframe>
+        </div>
+      </div>
 
+      <!-- projects window -->
+      <div class="windowBox" v-show="windowsOpen.projects" style="left:24vw; top:14vh;">
+        <div class="windowTitleBar">
+          <div class="windowTitleBarText">stuff i've made</div>
+          <div class="windowTitleBarControls">
+            <button @:click="toggleContent('projects')">
+              <div v-if="windowsContent.projects">_</div>
+              <div v-else>+</div>
+            </button>
+            <button @:click="closeWindow('projects')">X</button>
+          </div>
+        </div>
+        <!-- navbar -->
+        <nav v-show="windowsContent.projects" class="navbar">
+          <ul>
+            <li><a href="home.html" target="iframe_projects">web dev</a></li>
+            <li><a href="about.html" target="iframe_projects">game dev</a></li>
+            <li><a href="games.html" target="iframe_projects">other</a></li>
+            <li><a href="music.html" target="iframe_projects">contact</a></li>
+          </ul>
+        </nav>
+        <div v-show="windowsContent.projects" class="section-content" style="width:60vw;">
+          <!-- iframe! -->
+          <iframe src="home.html" name="iframe_projects" title="Projects Window IFrame"></iframe>
+        </div>
+      </div>
 
-    <div class="content">
-      <h1 class="h1">Hello BITCH </h1>
-      <button @click="alertme">
-        {{ num }}
-      </button>
+      <!-- warning popup window -->
+      <div class="windowBox" v-show="windowsOpen.warning">
+        <div class="windowTitleBar">
+          <div class="windowTitleBarText">warning!</div>
+          <div class="windowTitleBarControls">
+            <button @:click="closeWindow('warning')">X</button>
+          </div>
+        </div>
+        <div class="section-content">
+          <div style="display:flex; align-items: center; justify-content: space-between;">
+            <img src="@/assets/img/site/alert-icon.png"/>
+            <span>site under construction!</span>
+          </div>
+          <button @:click="closeWindow('warning')">ok</button>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -49,10 +159,9 @@ function alertme() {
 }
 
 
-function openWindow(id) {
-
+function openWindow(id)  {
 }
-
+          
 const num = ref(0)
 </script>
 
@@ -61,26 +170,38 @@ const num = ref(0)
 
 <!-- STYLE -->
 <style>
+@font-face{
+  font-family: Dogica;
+  src: url('@/assets/fonts/dogica.ttf');
+}
+
+@font-face{
+  font-family: Dogica;
+  src: url('@/assets/fonts/dogicabold.ttf');
+  font-weight: bold;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Dogica, Basiic;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background:  url('@/assets/img/bg/glitch1.png');
+  --text: #FFFFFF;
+  --bg: #101020;
+  --link: #A0D0FF;
+  
+  --header-image: url('@/assets/img/bg/glitch1.png');
+  --body-bg-image: url('@/assets/img/bg/glitch1.png');
+  --box-bg-image: url('@/assets/img/bg/glitch2.png');
+  --box2-bg-image: url('@/assets/img/bg/glitch2.png');
+  height: 100vh;
+  width: 100vw;
 }
 
-button {
-  padding: 10px 20px;
-  background-color: #42b983;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
 
-button:hover {
-  background-color: #38a673;
-}
+
 
 .app-container {
   display: flex;
@@ -110,4 +231,299 @@ button:hover {
   flex: 1;
   padding: 20px;
 }
+
+:root {
+  --text: #FFFFFF;
+  --bg: #101020;
+  --link: #A0D0FF;
+  
+  --header-image: url('@/assets/img/bg/glitch1.png');
+  --body-bg-image: url('@/assets/img/bg/glitch1.png');
+  --box-bg-image: url('@/assets/img/bg/glitch2.png');
+  --box2-bg-image: url('@/assets/img/bg/glitch2.png');
+}
+
+/* fonts */
+@font-face{
+  font-family: Dogica;
+  src: url('@/assets/fonts/dogica.ttf');
+}
+
+@font-face{
+  font-family: Dogica;
+  src: url('@/assets/fonts/dogicabold.ttf');
+  font-weight: bold;
+}
+
+* {
+  box-sizing: border-box; 
+}
+
+body {
+  background-color: var(--bg);
+  color: var(--text);
+  font-family: 'Dogica', 'MS PGothic', verdana, sans-serif;
+  margin: 0;
+  font-size: 12px;
+  line-height: 1.2;
+}
+
+/* index stuff */
+.index {
+  background-image: var(--body-bg-image);
+}
+
+iframe {
+  height:500px; 
+  width:100%;
+  border:none;
+}
+
+#container {
+  max-width: 900px;
+  margin: auto;
+  background-color: var(--bg);
+  border: 3px solid var(--text);
+}
+
+#container a{
+  color: var(--link);
+}
+#headerArea {
+  background-color: var(--bg);  
+}
+
+#header {
+  width: 100%;
+  background-color: var(--link);
+  color: var(--bg);
+  background-size: 100%;
+  padding: 5px;
+}
+
+/* navbar */
+.navbar {
+  /* navbar color */
+  color: var(--text);
+  background-color: var(--bg);
+  width: 100%;
+  margin: 0px; 
+}
+
+.navbar ul {
+  display: flex;
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+  justify-content: initial;
+}
+
+.navbar li {
+  padding: 10px;
+}
+
+
+.view {
+  background-image: var(--box-bg-image);
+}
+
+
+/* navigation links*/
+.navbar li a {
+  color: var(--text);
+  /* this removes the underline */
+  text-decoration: none;
+}
+
+.navbar li::first-letter {
+  text-decoration: underline; 
+}
+
+/* navigation link when a link is hovered over */
+.navbar li a:hover {
+  color: var(--link);
+}
+
+#flex {
+  display: flex;
+  background-image: var(--box-bg-image);
+  margin: 0px 5px;
+}
+
+#flex li {
+  margin: 5px;  
+}
+
+aside {
+  width: 250px;
+  margin: 20px 10px 20px 20px;
+}
+
+main {
+  flex: 1;
+  margin: 20px 20px 20px 10px;
+  order: 2;
+}
+
+#leftSidebar {
+  order: 1;
+}
+
+#rightSidebar {
+  order: 3;
+}
+
+footer {
+  background-color: var(--bg);
+  width: 40%;
+  height: 40px;
+  padding: 10px;
+  text-align: center;
+  color: var(--text);
+}
+
+ul {
+  padding-inline-start: 10px; 
+}
+
+h1,
+h2,
+h3 {
+  color: var(--text);
+}
+
+h1 {
+  font-size: 25px;
+}
+
+a {
+  color: var(--link); 
+}
+
+.box {
+  background-color: #42A584;
+  border: 1px solid #fceaff;
+  padding: 10px;
+}
+
+.box2 {
+  background-color: #AD73CE;
+  border: 1px solid #fceaff;
+  padding: 10px;
+}
+
+.section-header {
+  color: var(--bg);
+  background-color: var(--text);
+  padding: 5px;
+}
+
+.section-content {
+  color: var(--text);
+  background-color: var(--bg);
+  overflow: hidden;
+  padding: 10px;
+}
+
+
+/* window box stuff */
+.windowBox {
+  background-color: #AFDA9F;
+  border: 3px solid var(--text);
+  position: absolute;
+}
+
+.windowTitleBar {
+  color: var(--bg);
+  background-color: var(--link);
+  padding: 5px;
+  user-select: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.windowTitleBar:hover{
+  cursor: move;
+}
+
+.windowTitleBar button {
+  font-family: Dogica;
+  padding: 2px;
+  margin-left: 5px;
+  text-align: center;
+  color: var(--bg);
+  background-color: var(--link);
+  border: 2px solid var(--link);
+}
+
+.windowTitleBar button:hover{
+  background-color: var(--bg);
+  color: var(--link);
+  border: 2px solid var(--bg);
+  cursor: pointer;
+}
+
+.windowTitleBarControls {
+  display: flex;
+}
+
+.windowTitleBarText {
+  pointer-events: none;
+}
+
+/* desktop link stuff */
+.desktopLink {
+  margin-bottom: 20px;
+  padding: 5px;
+}
+
+.desktopLink:hover{
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 5px;
+}
+
+.desktopLink p {
+  margin-top: 0px;
+  user-select: none;
+}
+
+#topBar {
+  width: 100%;
+  height: 30px;
+  padding: 10px;
+  font-size: smaller;
+  background-color: #13092D;
+}
+
+@media only screen and (max-width: 800px) {
+  #flex {
+    flex-wrap: wrap;
+  }
+  
+  aside {
+    width: 100%;
+  }
+  
+  main {
+    order: 1;
+  }
+  
+  #leftSideBar {
+    order: 2; 
+  }
+  
+  #rightSideBar {
+    order: 3;
+  }
+  
+  .navbar ul {
+    flex-wrap: wrap; 
+  }
+}
+
+
+
+
 </style>
---
+++ src/components/ui/WindowComponent.vue
@@ -0,0 +1,65 @@
+<template>
+
+    <div class="windowBox" >
+      <section>
+        <div class="section-header">about</div>
+        <div class="section-content" style="overflow:hidden;">
+          <img style="float:left;margin:10px 40px;" src="img/site/uzu-glitch.png"/>
+          <ul>
+            <li><b>name: </b>melo toni</li>
+            <li><b>gender: </b>♂</li>
+            <li><b>age: </b>20 something</li>
+            <li><b>location: </b>usa</li>
+            <li><b>accolades: </b>winner of the my gf award for best bf 2024 (ran unopposed)</li>
+          </ul>
+        </div>
+  
+      </section>
+      
+      <p>
+      </p>
+      
+      <section>
+        <div class="section-header">stuff</div>
+        <div class="section-content">
+          <center>
+            <img src="https://plasticdino.neocities.org/buttons/hypnos.gif"/>
+            <img src="https://plasticdino.neocities.org/buttons/hitoshisan.gif"/>
+          </center>
+        </div>
+      </section>
+      <p></p>
+      <section>
+        <div class="section-header">blinkies</div>
+        <div class="section-content">
+          <center>
+            <img src="https://plasticdino.neocities.org/blinkie/b29.gif"/>
+            <img src="https://plasticdino.neocities.org/blinkie/fakegamer.gif"/>
+          </center>
+        </div>
+      </section>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+<style scoped>
+    .section-header {
+        color: var(--bg);
+        background-color: var(--text);
+        padding: 5px;
+      }
+      
+      .section-content {
+        color: var(--text);
+        background-color: var(--bg);
+        overflow: hidden;
+        padding: 10px;
+      }
+      
+      .windowBox {
+        left:20vw; top:10vh
+      }
+      
+</style>
